a2d21651cf93d468895490300bb744bf4d4e3be6
Priyanshu Upadhyay <62770722+Priyanshu-U@users.noreply.github.com>
2024-07-29 22:07:42 +0530

    Update 2_⭐️_About_Us.py

--- pages/2_⭐️_About_Us.py
+++ pages/2_⭐️_About_Us.py
@@ -1,4 +1,13 @@
 import streamlit as st
 
 st.sidebar.image("resources/WW_Logo.svg")
-st.title("About Us")
+st.title("About Wise Wealth")
+st.write("Wise Wealth is dedicated to empowering investors through the latest AI technologies, offering bespoke tools designed to provide deep insights and advanced analysis. Our mission is to help you navigate the complexities of the financial markets with confidence and precision.")
+
+st.subtitle("Our Upcoming Offerings:")
+
+st.write("StockX: Soon to be available, StockX will provide a comprehensive analysis of stocks, integrating fundamental data with news sentiment to offer a well-rounded view of each investment. This tool is designed to equip investors with the knowledge needed to make informed decisions.")
+
+st.write("StockWatch: Also coming soon, StockWatch will keep you ahead of the curve by delivering the most relevant and timely insights directly to your inbox. This tool ensures that you stay updated with crucial developments that can impact your investment strategy.")
+
+st.write("At Wise Wealth, we're committed to bringing these powerful tools to you, enhancing your investment decisions with advanced AI-driven analysis and insights.")
